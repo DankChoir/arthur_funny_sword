3419a8f4fcc8c5871dda9c381bd5204f63a6a373
DankChoir <timzek7@gmail.com>
2023-04-07 13:48:10 +0700

    fix KnightAdventure() in main

---
+++ knight2.cpp
@@ -0,0 +1,123 @@
+#include "knight2.h"
+
+//   _____                      _        
+//  |  ___|                    | |       
+//  | |__  __   __  ___  _ __  | |_  ___ 
+//  |  __| \ \ / / / _ \| '_ \ | __|/ __|
+//  | |___  \ V / |  __/| | | || |_ \__ \
+//  \____/   \_/   \___||_| |_| \__||___/
+
+Events::Events(const string& file_events){
+  ifstream events_file(file_events);
+  if(!events_file) cout << "dummy";
+  events_file >> this->numEvents;
+  eventCodes = new int[numEvents];
+  for(int i =0; i < numEvents;i++){
+    events_file >> this->eventCodes[i];
+  }
+  events_file.close();
+}
+
+int Events::count() const{
+  return this->numEvents;
+}
+
+int Events::get(int i) const {
+  return this->eventCodes[i];
+}
+
+Events::~Events(){
+  delete [] this->eventCodes;
+}
+
+//  ______                   ______               
+//  | ___ \                  | ___ \
+//  | |_/ /  __ _  ___   ___ | |_/ /  __ _   __ _ 
+//  | ___ \ / _` |/ __| / _ \| ___ \ / _` | / _` |
+//  | |_/ /| (_| |\__ \|  __/| |_/ /| (_| || (_| |
+//  \____/  \__,_||___/ \___|\____/  \__,_| \__, |
+//                                           __/ |
+
+
+//  ______                    _   __        _         _      _   
+//  | ___ \                  | | / /       (_)       | |    | |  
+//  | |_/ /  __ _  ___   ___ | |/ /  _ __   _   __ _ | |__  | |_ 
+//  | ___ \ / _` |/ __| / _ \|    \ | '_ \ | | / _` || '_ \ | __|
+//  | |_/ /| (_| |\__ \|  __/| |\  \| | | || || (_| || | | || |_ 
+//  \____/  \__,_||___/ \___|\_| \_/|_| |_||_| \__, ||_| |_| \__|
+//                                              __/ |            
+//    
+
+string BaseKnight::toString() const {
+    string typeString[4] = {"PALADIN", "LANCELOT", "DRAGON", "NORMAL"};
+    // inefficient version, students can change these code
+    //      but the format output must be the same
+    string s("");
+    s += "[Knight:id:" + to_string(id) 
+        + ",hp:" + to_string(hp) 
+        + ",maxhp:" + to_string(maxhp)
+        + ",level:" + to_string(level)
+        + ",gil:" + to_string(gil)
+        + "," + bag->toString()
+        + ",knight_type:" + typeString[knightType]
+        + "]";
+    return s;
+}
+
+//    ___                           _   __        _         _          
+//   / _ \                         | | / /       (_)       | |         
+//  / /_\ \ _ __  _ __ ___   _   _ | |/ /  _ __   _   __ _ | |__   ___ 
+//  |  _  || '__|| '_ ` _ \ | | | ||    \ | '_ \ | | / _` || '_ \ / __|
+//  | | | || |   | | | | | || |_| || |\  \| | | || || (_| || | | |\__ \
+//  \_| |_/|_|   |_| |_| |_| \__, |\_| \_/|_| |_||_| \__, ||_| |_||___/
+//                            __/ |                   __/ |            
+//                           |___/                   |___/             
+// void ArmyKnights::printInfo() const {
+//     cout << "No. knights: " << this->count();
+//     if (this->count() > 0) {
+//         BaseKnight * lknight = lastKnight(); // last knight
+//         cout << ";" << lknight->toString();
+//     }
+//     cout << ";PaladinShield:" << this->hasPaladinShield()
+//         << ";LancelotSpear:" << this->hasLancelotSpear()
+//         << ";GuinevereHair:" << this->hasGuinevereHair()
+//         << ";ExcaliburSword:" << this->hasExcaliburSword()
+//         << endl
+//         << string(50, '-') << endl;
+// }
+//
+// void ArmyKnights::printResult(bool win) const {
+//     cout << (win ? "WIN" : "LOSE") << endl;
+// }
+
+
+//   _     _         _         _              _______      _                                                 
+//  (_)   | |       (_)       | |      _     (_______)    | |                       _                        
+//   _____| | ____   _   ____ | |__  _| |_    _______   __| | _   _  _____  ____  _| |_  _   _   ____  _____ 
+//  |  _   _)|  _ \ | | / _  ||  _ \(_   _)  |  ___  | / _  || | | || ___ ||  _ \(_   _)| | | | / ___)| ___ |
+//  | |  \ \ | | | || |( (_| || | | | | |_   | |   | |( (_| | \ V / | ____|| | | | | |_ | |_| || |    | ____|
+//  |_|   \_)|_| |_||_| \___ ||_| |_|  \__)  |_|   |_| \____|  \_/  |_____)|_| |_|  \__)|____/ |_|    |_____)
+//                     (_____|                                                                               
+KnightAdventure::KnightAdventure() {
+    armyKnights = nullptr;
+    events = nullptr;
+}
+
+void KnightAdventure::loadEvents(const string &file_events){
+  events = new Events(file_events);
+}
+
+void KnightAdventure::run(){
+  // DEBUg
+  cout <<"So event la " << this->events->count();
+  for(int i =0; i < events->count();i++){
+    cout << events->get(i) << " ";
+  }
+  // DEBUG
+}
+
+KnightAdventure::~KnightAdventure() {
+    // delete armyKnights;
+    delete events;
+}
+
